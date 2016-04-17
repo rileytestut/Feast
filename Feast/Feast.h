f9bf8ad00136f59ec35525d2e26af2f646bb3bc8
Riley Testut <riley@rileytestut.com>
2016-04-17 03:56:11 -0700

    Initial Commit

---
+++ Feast/Feast.h
@@ -0,0 +1,19 @@
+//
+//  Feast.h
+//  Feast
+//
+//  Created by Riley Testut on 4/17/16.
+//  Copyright © 2016 Spark SC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Feast.
+FOUNDATION_EXPORT double FeastVersionNumber;
+
+//! Project version string for Feast.
+FOUNDATION_EXPORT const unsigned char FeastVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feast/PublicHeader.h>
+
+
